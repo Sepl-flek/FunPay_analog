1e8ea8a1e602a5aeefa59d65ef26077703195858
Sepl-flek <154527662+Sepl-flek@users.noreply.github.com>
2025-03-20 21:29:38 +0300

    add tests and custom Permission and auto seller definition

---
+++ Funpay/Product/tests/test_api.py
@@ -0,0 +1,54 @@
+import json
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from Product.models import Product
+from Product.serializers import ProductSerializer
+from user.models import CustomUser
+
+
+class ProductApiTestCase(APITestCase):
+
+    def setUp(self):
+        self.user1 = CustomUser.objects.create(username='test1')
+        self.user2 = CustomUser.objects.create(username='test2')
+
+        self.product1 = Product.objects.create(seller=self.user1, name='account', price=10, category='account')
+        self.product2 = Product.objects.create(seller=self.user1, name='account1', price=20, category='account')
+        self.product3 = Product.objects.create(seller=self.user1, name='account2', price=90.9, category='account')
+
+    def test_get(self):
+        url = reverse('product-list')
+
+        response = self.client.get(url)
+        products = Product.objects.all()
+        serializer_data = ProductSerializer(products, many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_update(self):
+        url = reverse('product-detail', args=(self.product1.pk, ))
+
+        data = {
+            'price': 20
+        }
+        json_data = json.dumps(data)
+
+        self.client.force_login(self.user1)
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.product1.refresh_from_db()
+        self.assertEqual(20, self.product1.price)
+
+    def test_update_logout(self):
+        url = reverse('product-detail', args=(self.product1.pk, ))
+
+        data = {
+            'price': 20
+        }
+        json_data = json.dumps(data)
+        response = self.client.patch(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+
--- Funpay/Product/views.py
+++ Funpay/Product/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
 from Product.models import Product
+from Product.permission import IsOwnerOrReadOnly
 from Product.serializers import ProductSerializer
 
 
 # Create your views here.
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all().select_related('seller')
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.validated_data['seller'] = self.request.user
+        serializer.save()
 
 
 def product_page(request):
